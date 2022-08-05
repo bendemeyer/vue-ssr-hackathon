a532760f79a719ed9f3cdf2175a75c9d92acc8b0
John Van Exan <john.vanexan@zoro.com>
2022-08-05 14:16:31 -0500

    added some template rendering

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 import express  from 'express';
 import path  from 'path';
 
+const lodash = require('lodash')
+const fs = require ('fs')
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.type('html').send('Hello again Worldie!😀')
+});
+
+app.get('/renderTemplate', (req, res) => {
+  const params = {
+    'param': req.query.param,
+    'subject': req.query.subject
+  }
+  const templateRoot = path.join(__dirname,'../html')
+  const fname = path.join(templateRoot, 'index.html') 
+  const htmlData = fs.readFileSync(fname)
+
+  const compiled = lodash.template(htmlData)
+  const toRender = compiled(params)
+  res.type('html').send(toRender)
 });
 
 app.use('/', express.static(path.join(__dirname, '../public')));
