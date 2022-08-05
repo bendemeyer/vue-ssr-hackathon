b6ee48b03e839954a711743727216263e931f1a0
Terence Ro <terence.ro@zoro.com>
2022-08-05 11:03:16 -0500

    accessing public dir

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import path from 'path';
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.type('html').send('Hello again Worldie!😀')
 })
+
+app.use('/static', express.static(path.join(path.dirname(import.meta.url), '../public')));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
