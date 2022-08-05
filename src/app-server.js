59549c0e886939b1067234ad6d7330f3c29213a3
Terence Ro <terence.ro@zoro.com>
2022-08-05 16:28:34 -0500

    changes

--- src/app-server.js
+++ src/app-server.js
@@ -1,14 +1,24 @@
 import express  from 'express';
 import path  from 'path';
+import cookieParser from 'cookie-parser';
+import { createSSRApp } from 'vue';
+import { renderToString } from 'vue/server-renderer';
+import vueApp from './app.vue';
 
 const lodash = require('lodash')
 const fs = require ('fs')
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 
-app.get('/', (req, res) => {
+app.use(cookieParser());
+
+
+app.get('/', async (req, res) => {
+  const vueSSRApp = createSSRApp(app);
+  const applicationHtml = await renderToString(vueSSRApp);
+  console.log(applicationHtml);
   const params = {
     'param': req.query.param,
     'subject': req.query.subject
@@ -20,6 +30,13 @@ app.get('/', (req, res) => {
   const compiled = lodash.template(htmlData)
   const toRender = compiled(params)
   res.type('html').send(toRender)
+});
+
+app.use((req, res, next) => {
+  if (!req.cookies.hackday) {
+    res.cookie('hackday', 'express');
+  }
+  next()
 });
 
 app.use('/', express.static(path.join(__dirname, '../../public')));
