0e0da843a107cf1994d164e7bd94e6d496d44ed6
Terence Ro <terence.ro@zoro.com>
2022-08-05 11:57:50 -0500

    configure webpack: ignore bundling node_modules, update nodemon script to point to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,31 @@
-const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+const path = require('path');
+
 module.exports = {
 	target: 'node',
 	entry: './src/app.js',
 	output: {
 		path: path.join(__dirname, 'dist'),
-	}
+		filename: 'bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			}
+		]
+	},
+	optimization: {
+	 	chunkIds: 'named',
+	 	moduleIds: 'deterministic',
+	 	concatenateModules: false,
+	 	splitChunks: false,
+	 	minimizer: [],
+	},
+	plugins: [
+		new CleanWebpackPlugin()
+	],
+	externals: [nodeExternals()],
 };
