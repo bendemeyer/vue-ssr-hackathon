e2f4085aa79aaa9dbd49c1ef4575da672c7f62c6
Brandon Kandarapally <brandon.kandarapally@zoro.com>
2022-10-14 16:02:16 -0500

    404 Not Found Handle

---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'PageNotFound',
+}
+</script>
+
+<template>
+  <div>
+    <h1>404 NOT FOUND</h1>
+    <img src="https://media.tenor.com/lx2WSGRk8bcAAAAC/pulp-fiction-john-travolta.gif">
+    <router-link to="/">back home</router-link>
+  </div>
+</template>
+
+<style>
+.home-content {
+  color: #0000c0;
+}
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import Foo from './pages/Foo.vue';
 import Bar from './pages/Bar.vue';
 import Home from './pages/Home.vue';
+import PageNotFound from './pages/PageNotFound.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/foo', component: Foo },
   { path: '/bar', component: Bar },
+  {path: '/:pathMatch(.*)*', component: PageNotFound}
 ];
 
 export default routes;
