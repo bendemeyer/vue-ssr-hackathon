61b9d989e19bec767ad753cc020c92e29ce3d974
Terence Ro <terence.ro@zoro.com>
2023-05-26 16:04:25 -0500

    build manifest

--- chunk-plugin.js
+++ chunk-plugin.js
@@ -29,8 +29,39 @@ class ChunkPlugin {
   }
 
   buildStatsFile(compilation) {
+    const stats = compilation.getStats().toJson();
     const manifest = {};
 
+    // 1. Chunk names for entrypoint chunk(s)
+    manifest.initialChunks = stats.chunks.filter((chunk) => chunk.initial).map((chunk) => chunk.id);
+
+    // 2. Map of which file(s) to load for each chunk
+    //    Contains both JS, CSS, and any additional assets that should be preloaded
+    manifest.chunkFilesMap = {};
+    stats.chunks.forEach((chunk) => {
+      const chunkFiles = chunk.files.concat(chunk.auxiliaryFiles);
+      if (chunkFiles.length) {
+        manifest.chunkFilesMap[chunk.id] = chunkFiles;
+      }
+    });
+
+    // 3. Map of which modules exist in which chunk
+    manifest.componentChunkMap = {};
+    stats.modules.forEach((module) => {
+      const id = stripModuleIdHash(module.identifier);
+      // only vue files get tagged by vue-loader, and ignore the other sub-modules (per SFC block type) spun off by it
+      if (id.includes('vue-loader') && !id.includes('type=')) {
+        const hashId = hash(id); // IMPORTANT! same hashing as vue-loader
+        manifest.componentChunkMap[hashId] = module.chunks;
+      }
+    });
+
+    // 4. Tell webpack to write the manifest
+    const manifestJson = JSON.stringify(manifest, null, 2);
+    compilation.assets[`ssr-manifest.json`] = {
+      source: () => manifestJson,
+      size: () => manifestJson.length,
+    };
   }
 }
 
