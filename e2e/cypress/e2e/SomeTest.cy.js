3f20fccaaa597d0f2e4b1988e270679ae1f37e62
Val Trubachev <val.trubachev@zoro.com>
2023-08-08 13:40:43 -0500

    cy request test

--- e2e/cypress/e2e/SomeTest.cy.js
+++ e2e/cypress/e2e/SomeTest.cy.js
@@ -1,4 +1,10 @@
 describe('some test', () => {
+  it('should try?', () => {
+    cy.request('/').then((response) => {
+      console.log('hi body', request.body);
+    });
+  });
+
   it('should pass', () => {
     cy.visit('/');
 
